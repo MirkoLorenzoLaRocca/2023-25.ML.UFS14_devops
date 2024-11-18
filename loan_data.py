16fe94324732438196376a4578a0c96551bd1138
fvdfab <dsrdbdf>
2024-11-18 15:54:23 +0000

    update

--- loan_data.py
+++ loan_data.py
@@ -25,33 +25,9 @@ def pippo(path):
     plt.savefig(output_dir + "/box_plot.png")  # Save instead of showing
     plt.close()
 
-    # Prepare the data
-    df_binary = df.copy()
-    df_binary['person_gender'] = df['person_gender'].astype(str).str.strip().str.lower().map({'female': 0, 'male': 1})
-    df_binary['previous_loan_defaults_on_file'] = df['previous_loan_defaults_on_file'].map({'Yes': 1, 'No': 0})
     
-    # One hot encoding
-    df_encoded = pd.get_dummies(df_binary, columns=['person_education'], prefix='edu', dtype=int)
-    df_encoded['edu_High_School'] = df_encoded['edu_High School']
-    df_encoded = df_encoded.drop(columns=['edu_High School'])
-    df_encoded = pd.get_dummies(df_encoded, columns=['person_home_ownership'], prefix="home_own", dtype=int)
-    df_encoded = pd.get_dummies(df_encoded, columns=['loan_intent'], dtype=int)
 
-    # Scaling
-    columns_to_scale = [
-        'person_age', 'person_income', 'loan_amnt', 'credit_score', 'loan_int_rate'
-    ]
-    scaler = StandardScaler()
-    df_scaled = df_encoded.copy()
-    df_scaled[columns_to_scale] = scaler.fit_transform(df_encoded[columns_to_scale])
-
-    # Train-test split
-    from sklearn.model_selection import train_test_split
-    y = df_scaled['loan_status']
-    x = df_scaled.drop(['loan_status'], axis=1)
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
-    # Logistic Regression
+   """ # Logistic Regression
     lr = LogisticRegression(max_iter=10000)
     lr.fit(X_train, y_train)
     
@@ -61,9 +37,7 @@ def pippo(path):
     weights['Weights'] = lr.coef_.reshape(-1)
     weights.sort_values(by='Weights', ascending=False).to_csv("logistic_regression_weights.csv", index=False)
 
-    # Random Forest Classifier
-    rf = RandomForestClassifier()
-    rf.fit(X_train, y_train)
+   
 
     # XGBoost Classifier
     xgb = XGBClassifier()
@@ -75,7 +49,7 @@ def pippo(path):
     plt.barh(x.columns[sorted_idx], xgb.feature_importances_[sorted_idx])
     plt.xlabel('XGBoost feature importance')
     plt.savefig(output_dir + "/xgboost_feature_importance.png")  # Save instead of showing
-    plt.close()
+    plt.close()"""
 
     # Contingency Table Plot
     contingency_table = pd.crosstab(df['previous_loan_defaults_on_file'], df['loan_status'])
